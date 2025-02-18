88cc8548564852d301e3ce7cca1b800cc92563d0
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-02-18 09:20:07 +0000

    Add Kafka producer integration to CreateEmployee endpoint and update dependencies

--- EmployeeApplicationApi/Controllers/EmployeesController.cs
+++ EmployeeApplicationApi/Controllers/EmployeesController.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Confluent.Kafka;
 using EmployeeApplicationApi.Database;
 using EmployeeApplicationApi.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,18 +16,18 @@ namespace EmployeeApplicationApi.Controllers
         private readonly ILogger<EmployeesController> _logger;
         private readonly EmployeeDbContext _dbContext;
 
-        public EmployeesController(EmployeeDbContext dbContext , ILogger<EmployeesController> logger)
+        public EmployeesController(EmployeeDbContext dbContext, ILogger<EmployeesController> logger)
         {
             _logger = logger;
             _dbContext = dbContext;
-        }   
+        }
 
         [HttpGet]
 
         public async Task<IEnumerable<Employee>> GetEmployees()
         {
-            _logger. LogInformation("Requesting all employees");
-           return await  _dbContext.Employees.ToListAsync();
+            _logger.LogInformation("Requesting all employees");
+            return await _dbContext.Employees.ToListAsync();
 
         }
 
@@ -34,9 +36,37 @@ namespace EmployeeApplicationApi.Controllers
         {
             var employee = new Employee(Guid.NewGuid(), name, surname);
             _dbContext.Employees.Add(employee);
-           await _dbContext.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
 
-           return CreatedAtAction(nameof(CreateEmployee), new {id = employee.Id}, employee);    
+            var message = new Message<Guid, string>()
+            {
+                Key = employee.Id,
+                Value = JsonSerializer.Serialize(employee)
+            };
+
+            //clients
+            var producerConfig = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+                Acks = Acks.All
+                //you can add it to be like Acks.All, Acks.None, Acks.Leader, Acks.Local 
+                /*Acks.All: This means the leader will wait for the full set of in-sync replicas to acknowledge the record.
+                This guarantees that the record will not be lost as long as at least one in-sync replica remains alive. This is the strongest available guarantee.
+                Acks.None: This means the producer will not wait for any acknowledgment from the server at all. The record will be immediately added to the socket buffer and considered sent. No guarantee can be made that the server has received the record in this case, and the retries configuration will not take effect (as the client won't generally know of any failures).
+                Acks.Leader: This means the leader will write the record to its local log but will respond without awaiting full acknowledgment from all followers. In this case should the leader fail immediately after acknowledging the record but before the followers have replicated it then the record will be lost.
+                Acks.Local: This means the leader will wait for the full set of in-sync replicas to acknowledge the record only after all in-sync replicas have acknowledged the record. This guarantees that the record will not be lost as long as at least one in-sync replica remains alive and that the record will be acknowledged by the full set of in-sync replicas.
+                */
+
+            };
+
+            var producer = new ProducerBuilder<Guid, string>(producerConfig).Build();
+           await  producer.ProduceAsync("", message );
+           producer.Dispose()
+            ;
+
+            //broker 1, broker 2, broker 3 (In sync replicas)
+
+            return CreatedAtAction(nameof(CreateEmployee), new { id = employee.Id }, employee);
 
         }
 
